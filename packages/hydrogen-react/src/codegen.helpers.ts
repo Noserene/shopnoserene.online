274c3c383886e454cff6905eff25b1e54adcb727
noserene <101975357+Zoowizz@users.noreply.github.com>
2024-06-05 14:50:05 -0500

    Initialize parse-metaobject

---
+++ packages/hydrogen-react/src/codegen.helpers.ts
@@ -0,0 +1,28 @@
+/**
+ * Meant to be used with GraphQL CodeGen to type the Storefront API's custom scalars correctly.
+ * Reference for the GraphQL types: https://shopify.dev/docs/api/storefront/current/scalars/HTML
+ * Note: JSON is generated as 'unknown' by default.
+ */
+export const storefrontApiCustomScalars = {
+  // Keep in sync with the definitions in the app/nextjs/codegen.ts!
+  DateTime: 'string',
+  Decimal: 'string',
+  HTML: 'string',
+  URL: 'string',
+  Color: 'string',
+  UnsignedInt64: 'string',
+};
+
+/**
+ * Meant to be used with GraphQL CodeGen to type the Storefront API's custom scalars correctly.
+ * Reference for the GraphQL types: https://shopify.dev/docs/api/customer/current/scalars/HTML
+ * Note: JSON is generated as 'unknown' by default.
+ */
+export const customerApiCustomScalars = {
+  DateTime: 'string',
+  Decimal: 'string',
+  HTML: 'string',
+  ISO8601DateTime: 'string',
+  URL: 'string',
+  UnsignedInt64: 'string',
+};
