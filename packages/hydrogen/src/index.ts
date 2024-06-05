463ec53e22bc859b3fcfa0155226fa100112e1a9
noserene <101975357+Zoowizz@users.noreply.github.com>
2024-06-05 14:47:40 -0500

    Initialize bl-test-oxygen

---
+++ packages/hydrogen/src/index.ts
@@ -0,0 +1,94 @@
+export * from './storefront';
+export * from './with-cache';
+export {
+  CacheCustom,
+  CacheLong,
+  CacheNone,
+  CacheShort,
+  generateCacheControlHeader,
+} from './cache/strategies';
+export {InMemoryCache} from './cache/in-memory';
+
+export {storefrontRedirect} from './routing/redirect';
+export {graphiqlLoader} from './routing/graphiql';
+export {Seo} from './seo/seo';
+export {type SeoConfig} from './seo/generate-seo-tags';
+export type {SeoHandleFunction} from './seo/seo';
+export {
+  Pagination as Pagination__unstable,
+  getPaginationVariables as getPaginationVariables__unstable,
+} from './pagination/Pagination';
+
+export {CartForm, type CartActionInput} from './cart/CartForm';
+export {cartCreateDefault} from './cart/queries/cartCreateDefault';
+export {cartGetDefault} from './cart/queries/cartGetDefault';
+export {cartLinesAddDefault} from './cart/queries/cartLinesAddDefault';
+export {cartLinesUpdateDefault} from './cart/queries/cartLinesUpdateDefault';
+export {cartLinesRemoveDefault} from './cart/queries/cartLinesRemoveDefault';
+export {cartDiscountCodesUpdateDefault} from './cart/queries/cartDiscountCodesUpdateDefault';
+export {cartBuyerIdentityUpdateDefault} from './cart/queries/cartBuyerIdentityUpdateDefault';
+export {cartNoteUpdateDefault} from './cart/queries/cartNoteUpdateDefault';
+export {cartSelectedDeliveryOptionsUpdateDefault} from './cart/queries/cartSelectedDeliveryOptionsUpdateDefault';
+export {cartAttributesUpdateDefault} from './cart/queries/cartAttributesUpdateDefault';
+export {cartMetafieldsSetDefault} from './cart/queries/cartMetafieldsSetDefault';
+export {cartMetafieldDeleteDefault} from './cart/queries/cartMetafieldDeleteDefault';
+export {cartGetIdDefault} from './cart/cartGetIdDefault';
+export {cartSetIdDefault} from './cart/cartSetIdDefault';
+export {
+  type HydrogenCartCustom,
+  type HydrogenCart,
+  createCartHandler,
+} from './cart/createCartHandler';
+export type {
+  MetafieldWithoutOwnerId,
+  CartQueryData,
+  CartQueryOptions,
+  CartQueryReturn,
+} from './cart/queries/cart-types';
+
+export {
+  VariantSelector as VariantSelector__unstable,
+  getSelectedProductOptions as getSelectedProductOptions__unstable,
+  getFirstAvailableVariant as getFirstAvailableVariant__unstable,
+} from './product/VariantSelector';
+
+export {
+  AnalyticsEventName,
+  AnalyticsPageType,
+  ExternalVideo,
+  flattenConnection,
+  getClientBrowserParameters,
+  getShopifyCookies,
+  Image,
+  IMAGE_FRAGMENT,
+  MediaFile,
+  ModelViewer,
+  Money,
+  parseGid,
+  parseMetafield,
+  sendShopifyAnalytics,
+  ShopifySalesChannel,
+  ShopPayButton,
+  storefrontApiCustomScalars,
+  useMoney,
+  useShopifyCookies,
+  Video,
+} from '@shopify/hydrogen-react';
+
+export type {
+  ClientBrowserParameters,
+  ParsedMetafields,
+  ShopifyAddToCart,
+  ShopifyAddToCartPayload,
+  ShopifyAnalytics,
+  ShopifyAnalyticsPayload,
+  ShopifyAnalyticsProduct,
+  ShopifyCookies,
+  ShopifyPageView,
+  ShopifyPageViewPayload,
+  StorefrontApiResponse,
+  StorefrontApiResponseError,
+  StorefrontApiResponseOk,
+  StorefrontApiResponseOkPartial,
+  StorefrontApiResponsePartial,
+} from '@shopify/hydrogen-react';
---
+++ templates/demo-store/app/root.tsx
@@ -0,0 +1,81 @@
+import {type LinksFunction, type LoaderArgs, json} from '@shopify/remix-oxygen';
+import {
+  isRouteErrorResponse,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  useMatches,
+  useRouteError,
+} from '@remix-run/react';
+
+import favicon from '../public/favicon.svg';
+
+import styles from './styles/app.css';
+
+export const links: LinksFunction = () => {
+  return [
+    {rel: 'stylesheet', href: styles},
+    {
+      rel: 'preconnect',
+      href: 'https://cdn.shopify.com',
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://shop.app',
+    },
+    {rel: 'icon', type: 'image/svg+xml', href: favicon},
+  ];
+};
+
+export async function loader({request, context}: LoaderArgs) {
+  return json({});
+}
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary({error}: {error: Error}) {
+  const [root] = useMatches();
+  const locale = root?.data?.selectedLocale ?? 'us';
+  const routeError = useRouteError();
+  const isRouteError = isRouteErrorResponse(routeError);
+
+  let title = 'Error';
+  let pageType = 'page';
+
+  if (isRouteError) {
+    title = 'Not found';
+    if (routeError.status === 404) pageType = routeError.data || pageType;
+  }
+
+  return (
+    <html lang={locale.language}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>{title}</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>Error!</h1>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
