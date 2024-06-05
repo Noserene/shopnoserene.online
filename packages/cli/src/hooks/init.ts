b756f9cf6e2c7053bac3bc0bd75014bcfdb24438
noserene <101975357+Zoowizz@users.noreply.github.com>
2024-06-05 14:45:15 -0500

    Initial commit

---
+++ packages/cli/src/hooks/init.ts
@@ -0,0 +1,35 @@
+import {spawnSync} from 'child_process';
+import {outputDebug} from '@shopify/cli-kit/node/output';
+import type {Hook} from '@oclif/core';
+
+const EXPERIMENTAL_VM_MODULES_FLAG = '--experimental-vm-modules';
+
+function commandNeedsVM(id = '', argv: string[] = []) {
+  return (
+    // All the commands that rely on MiniOxygen's Node sandbox:
+    ['hydrogen:dev', 'hydrogen:preview', 'hydrogen:debug:cpu'].includes(id) &&
+    !argv.includes('--worker')
+  );
+}
+
+const hook: Hook<'init'> = async function (options) {
+  if (
+    commandNeedsVM(options.id, options.argv) &&
+    !process.execArgv.includes(EXPERIMENTAL_VM_MODULES_FLAG) &&
+    !(process.env.NODE_OPTIONS ?? '').includes(EXPERIMENTAL_VM_MODULES_FLAG)
+  ) {
+    outputDebug(
+      `Restarting CLI process with ${EXPERIMENTAL_VM_MODULES_FLAG} flag.`,
+    );
+
+    const [command, ...args] = process.argv;
+    args.unshift(EXPERIMENTAL_VM_MODULES_FLAG);
+
+    spawnSync(command!, args, {stdio: 'inherit'});
+
+    // This code should not be reached
+    process.exit(0);
+  }
+};
+
+export default hook;
