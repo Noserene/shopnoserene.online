9ba8ab8acc3817ecc14b8017ab9bced36208be89
noserene <101975357+Zoowizz@users.noreply.github.com>
2024-06-05 14:48:34 -0500

    Initialize bl-remove-sitemap

---
+++ templates/demo-store/app/routes/[robots.txt].tsx
@@ -0,0 +1,37 @@
+import {type LoaderArgs} from '@shopify/remix-oxygen';
+
+export const loader = ({request}: LoaderArgs) => {
+  const url = new URL(request.url);
+
+  return new Response(robotsTxtData(), {
+    status: 200,
+    headers: {
+      'content-type': 'text/plain',
+      // Cache for 24 hours
+      'cache-control': `max-age=${60 * 60 * 24}`,
+    },
+  });
+};
+
+function robotsTxtData() {
+  return `
+User-agent: *
+Disallow: /admin
+Disallow: /cart
+Disallow: /orders
+Disallow: /checkouts/
+Disallow: /checkout
+Disallow: /carts
+Disallow: /account
+
+# Google adsbot ignores robots.txt unless specifically named!
+User-agent: adsbot-google
+Disallow: /checkouts/
+Disallow: /checkout
+Disallow: /carts
+Disallow: /orders
+
+User-agent: Pinterest
+Crawl-delay: 1
+`.trim();
+}
