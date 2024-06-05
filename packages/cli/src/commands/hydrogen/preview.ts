d429a748372da4f8a25d0c7c963e1466da0f98f7
noserene <101975357+Zoowizz@users.noreply.github.com>
2024-06-05 14:46:33 -0500

    Initialize fd-remix-hmr

---
+++ packages/cli/src/commands/hydrogen/preview.ts
@@ -0,0 +1,43 @@
+import Command from '@shopify/cli-kit/node/base-command';
+import {muteDevLogs} from '../../utils/log.js';
+import {getProjectPaths} from '../../utils/config.js';
+import {commonFlags, DEFAULT_PORT} from '../../utils/flags.js';
+import {startMiniOxygen} from '../../utils/mini-oxygen.js';
+import {findPort} from '../../utils/find-port.js';
+
+export default class Preview extends Command {
+  static description =
+    'Runs a Hydrogen storefront in an Oxygen worker for production.';
+  static flags = {
+    path: commonFlags.path,
+    port: commonFlags.port,
+  };
+
+  async run(): Promise<void> {
+    // @ts-ignore
+    const {flags} = await this.parse(Preview);
+
+    await runPreview({...flags});
+  }
+}
+
+export async function runPreview({
+  port = DEFAULT_PORT,
+  path: appPath,
+}: {
+  port?: number;
+  path?: string;
+}) {
+  if (!process.env.NODE_ENV) process.env.NODE_ENV = 'production';
+
+  muteDevLogs({workerReload: false});
+
+  const {root, buildPathWorkerFile, buildPathClient} = getProjectPaths(appPath);
+
+  await startMiniOxygen({
+    root,
+    port: await findPort(port),
+    buildPathClient,
+    buildPathWorkerFile,
+  });
+}
