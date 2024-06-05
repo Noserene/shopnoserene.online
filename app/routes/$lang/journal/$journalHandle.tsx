a3713b2951fa904f8e4ea3b6803192856895d101
noserene <101975357+Zoowizz@users.noreply.github.com>
2024-06-05 14:45:23 -0500

    Initialize @juanpprieto/global-provider

---
+++ app/routes/$lang/journal/$journalHandle.tsx
@@ -0,0 +1,9 @@
+import Component, {
+  loader,
+  meta,
+  links,
+  CatchBoundary,
+} from '~/routes/journal/$journalHandle';
+
+export {loader, meta, links, CatchBoundary};
+export default Component;
---
+++ worker/index.ts
@@ -0,0 +1,31 @@
+import {createRequestHandler} from '@hydrogen/remix';
+// The build remix app provided by remix build
+import * as remixBuild from 'remix-build';
+
+const requestHandler = createRequestHandler({
+  build: remixBuild,
+  mode: process.env.NODE_ENV,
+  storefront: {
+    publicStorefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'hydrogen-preview',
+    storefrontApiVersion: '2022-07',
+  },
+});
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext,
+  ): Promise<Response> {
+    try {
+      return await requestHandler(request, {
+        env,
+        ctx,
+      });
+    } catch (error) {
+      console.error(error);
+      return new Response('An unexpected error occurred', {status: 500});
+    }
+  },
+};
